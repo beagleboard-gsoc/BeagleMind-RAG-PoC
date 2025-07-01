dcb576385c4aaa3e268201cd8c1014091ff9d775
fayezzouari <fayez.zouari@insat.ucar.tn>
2025-07-01 16:03:11 +0100

    feat: made a notebook to process the links and scrape them + stored forum content in the collection

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -15,7 +15,7 @@ GROQ_MODELS = [
     "llama-3.3-70b-versatile",
     "llama-3.1-8b-instant",
     "gemma2-9b-it",
-    "deepseek-r1-distill-llama-70b",
+    "meta-llama/llama-4-scout-17b-16e-instruct",
     "meta-llama/llama-4-maverick-17b-128e-instruct"
 ]
 
